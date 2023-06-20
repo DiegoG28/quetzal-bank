d130c2cbab50185c73d20679dff86e144aea49e9
Diego <diegogutcat28@gmail.com>
2023-06-19 20:02:50 -0500

    Second commit

---
+++ QuetzalBank/QuetzalBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuetzalBankApp.swift
+//  QuetzalBank
+//
+//  Created by Diego Gutiérrez on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct QuetzalBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
